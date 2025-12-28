a96845588bca2ea6e39076f1241c248afddef057
Ahmed Hassop <Ahmed Hassob>
2025-12-28 18:52:28 +0200

    add lesson 4

--- desc.js
+++ desc.js
@@ -1,3 +1,27 @@
-// 1-What is useQuery
-// 2-Query Key
-// 3-Query Function
+//? 1-Query Options
+// useQuery(groupOptions(1))
+// useSuspenseQuery(groupOptions(5))
+// useQueries({
+//     queries: [groupOptions(1), groupOptions(2)],
+// })
+
+// ####################################
+
+//? 2-Dependent Queries
+//! In First
+// status: 'pending'
+// isPending: true
+// fetchStatus: 'idle'
+// As soon as the user is available, the projects query will be enabled and will then transition to:
+//! In Second
+// status: 'pending'
+// isPending: true
+// fetchStatus: 'fetching'
+// Once we have the projects, it will go to:
+//! In Third
+// status: 'success'
+// isPending: false
+// fetchStatus: 'idle'
+
+
+
--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,57 @@
-import { useQuery } from "@tanstack/react-query"
+import { queryOptions, useQueries, useQuery, useSuspenseQuery } from "@tanstack/react-query"
 
 export default function App() {
 
-  const { data, isLoading, isError } = useQuery({
-    queryKey: ['ClientPosts'],
-    queryFn: async () => {
-      const response = await fetch('http://localhost:3000/posts')
-      return response.json()
-    }
-    // queryFn: () => getPost()
-    // queryFn: getPost
-  })
+  // const { data, isLoading, isError } = useQuery({
+  //   queryKey: ['ClientPosts'],
+  //   queryFn: async () => await getPost()
+  // })
+
+  function groups(id) {
+    return queryOptions({
+      queryKey: [id],
+      queryFn: async () => await getPost()
+    })
+  }
+
+  function groupsComments() {
+    return queryOptions({
+      queryKey: ["comments"],
+      queryFn: async () => await getComments()
+    })
+  }
 
   // Get Post Function
-  // async function getPost() {
-  //   const response = await fetch('http://localhost:3000/posts')
-  //   return response.json()
-  // }
+  async function getPost() {
+    const response = await fetch('http://localhost:3000/posts')
+    return response.json()
+  }
+  // Get Comments Function
+  async function getComments() {
+    const response = await fetch('http://localhost:3000/comments')
+    return response.json()
+  }
+
+  const { data, isLoading } = useQuery(groups(1))
+
+  const suspenseData = useSuspenseQuery(groups(5))
+
+  const queriesData = useQueries({
+    queries: [groups(1), groupsComments()],
+  })
+
+  console.log(queriesData[1].data)
+  console.log(suspenseData.data)
 
   if (isLoading) {
     return <div>Loading...</div>
   }
 
-  if (isError) {
-    alert("Worng")
-  }
+  // if (isError) {
+  //   alert("Worng")
+  // }
 
-  console.log(data)
+  // console.log(data)
 
   return (
     <>
