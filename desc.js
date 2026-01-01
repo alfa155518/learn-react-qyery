e680f2c0c2232dec20d78bb76291766708478f5e
Ahmed Hassop <Ahmed Hassob>
2026-01-01 22:51:16 +0200

    Add: add lesson-7

--- desc.js
+++ desc.js
@@ -1,27 +1,2 @@
-//? 1-refetchOnWindowFocus
-// If a user leaves your application and returns and the query data is stale, TanStack Query automatically requests fresh data for you in the background. You can disable this globally or per-query .
-
-//! Example:
-// refetchOnWindowFocus: false,
-// defaultOptions: {
-//     queries: {
-//         refetchOnWindowFocus: false, // default: true
-//     },
-// },
-
-
-//? 2-Disabling/Pausing Queries
-
-
-//? 3-retry
-// When a useQuery query fails(the query function throws an error), TanStack Query will automatically retry the query if that query's request has not reached the max number of consecutive retries (defaults to 3)
-
-//*1 Setting retry = false will disable retries.
-//*2 Setting retry = 6 will retry failing requests 6 times before showing the final error thrown by the function.
-//*3 Setting retry = true will infinitely retry failing requests.
-//*4 Setting retry = (failureCount, error) => ... allows for custom logic based on why the request failed.
-
-//? 4-retryDelay
-// By default, retries in TanStack Query do not happen immediately after a request fails.As is standard, a back - off delay is gradually applied to each retry attempt.
-
-// The default retryDelay is set to double(starting at 1000ms) with each attempt
+//? 1-Placeholder Query Data
+// Placeholder data allows a query to behave as if it already has data, similar to the initialData option, but the data is not persisted to the cache.This comes in handy for situations where you have enough partial(or fake) data to render the query successfully while the actual data is fetched in the background.
--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,41 @@
 import { useQuery } from "@tanstack/react-query"
 
 
+
 export default function App() {
 
+  const placeholderPosts = [
+    { "id": "1", "title": "placeholderPosts title", "views": 100 },
+    { "id": "2", "title": "placeholderPosts another title", "views": 200 },
+  ]
 
   // Get Post Function
   async function getPost() {
-    const response = await fetch('http://localhost:3000/posts')
-    return response.json()
-  }
-
-  // Get Comments Function
-  async function getComments() {
-    const response = await fetch('http://localhost:3000/comments')
+    const response = await fetch(`http://localhost:3000/posts`)
     return response.json()
   }
 
 
-  const { data, isLoading, isFetching, isError } = useQuery({
+  const { data, isLoading, isError, isPlaceholderData } = useQuery({
     queryKey: ['ClientPosts'],
     queryFn: async () => await getPost(),
-    // refetchOnWindowFocus: false,
-    retry: 5
+    placeholderData: placeholderPosts
   })
 
-
-
-  const { data: commentsData, refetch } = useQuery({
-    queryKey: ['ClientComments'],
-    queryFn: async () => await getComments(),
-    // Disabling/Pausing Queries
-    enabled: false
-  })
 
 
   if (isLoading) {
     return <div>Loading...</div>
   }
+
+  // To Ensure it is placeholder data
+  // if (isPlaceholderData) {
+  //   return <div>Loading Placeholder Data...</div>
+  // }
+
   if (isError) {
     return <div>Ther is Error...</div>
   }
-
-
 
   return (
     <>
@@ -55,11 +49,6 @@ export default function App() {
         })
       }
       <div>
-        {
-          isFetching ? <div>Loading comments...</div> : commentsData?.map(comment => <p key={comment.id}>{comment.text}</p>)
-        }
-        {/* Show When use Disabling/Pausing Queries*/}
-        <button onClick={() => refetch()}>Refetch</button>
       </div>
     </>
   )
